5c16586ae63f2cd7b65d4dc9d659c0575ad912f7
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-05-09 13:45:51 -0400

    Test push
    This commit is for checking if the project will properly save to Github

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,51 @@
+package com.LickingHeights;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Variables
+        String input;
+        boolean restart = true;
+
+        //Whole game
+        Scanner choice = new Scanner(System.in);
+        System.out.println("Rock, Paper, Scissors, what is your choice? ");
+        input = choice.nextLine();
+
+
+
+
+        //Test code here
+        String[] thePossibleChoices = {"Rock", "Paper", "Scissors"}; // Test for trying out arrays
+
+
+    }
+
+    public static String choices(String rock, String paper, String scissors){
+
+
+        if (){
+
+
+        }
+
+
+
+    }
+
+    public static String results(){
+
+
+    }
+
+
+
+    public static int calculations(){
+
+
+
+    }
+
+
+}
