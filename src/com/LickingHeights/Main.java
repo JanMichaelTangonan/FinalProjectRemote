d3d898f1397cb354daf7f0fcb02ede2da0f84af4
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-05-20 14:19:39 -0400

    Changed order of codes and re implemented scoring system

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -17,12 +17,9 @@ public class Main {
         // Variables for Results
         int playerScore = 0; // The score of the player/user
         int computerScore = 0; // The score of the computer
-        //int playerWinOrLose = 0; // *Supposedly "outcome" of the round must find this for the game to work(This piece of code may be useless...if it is delete it)
-        boolean coreGameLoop = true;
-        boolean playerWinRound=true;
 
 
-        // Computer choice
+        // Choices for both player and computer
         String choices[] = new String[3];
         choices[0] = "Rock";
         choices[1] = "Paper";
@@ -30,49 +27,40 @@ public class Main {
 
 
         // Core game
-        while (coreGameLoop==true) {
-            do {
-                choiceOfComputer = new Random().nextInt(choiceComputer.length);
 
-                Scanner choice = new Scanner(System.in);
-                System.out.println("Rock, Paper, Scissors what is your choice? (0 = Rock, 1 = Paper, 2 = Scissors)");
-                input = choice.nextInt();
+        //do {
+        do {
+            choiceOfComputer = new Random().nextInt(choices.length);
 
-                // Round Results
-                System.out.println("You have chosen " + choices[input]);
-                System.out.println("The computer has chosen " + choices[choiceOfComputer]);
+            Scanner choice = new Scanner(System.in);
+            System.out.println("Rock, Paper, Scissors what is your choice? (0 = Rock, 1 = Paper, 2 = Scissors)");
+            input = choice.nextInt();
 
-
-                // Results
-
-                //Player wins round
-                if (playerWinRound==true){
-
-                    System.out.print(playerWins(input, choiceOfComputer, playerScore));
-                    playerScore++;
-
-                }
-               // Computer wins round
-                if(playerWinRound==false){
-
-                System.out.println(computerWins(input,choiceOfComputer,computerScore)) ;
-                computerScore++;
-                }
+            // Round Results
+            System.out.println("You have chosen " + choices[input]);
+            System.out.println("The computer has chosen " + choices[choiceOfComputer]);
 
 
+            // Results
 
-                // Results for a tie
-                if (input == choiceOfComputer) {
+            //Player wins round
+            playerWins(input, choiceOfComputer, playerScore);
 
-                    System.out.println("Its a tie! Keep going!");
-                }
+            // Computer wins round
 
-
-
+            computerWins(input, choiceOfComputer, playerScore);
 
 
-            }while (playerScore != 10 || computerScore != 10) ;
-        }
+            // Results for a tie
+            if (input == choiceOfComputer) {
+
+                System.out.println("Its a tie! Keep going!");
+            }
+
+
+        } while (playerScore != 10 || computerScore != 10);
+
+        //}while (coreGameLoop==true);
 
 
         // Final result
@@ -87,59 +75,31 @@ public class Main {
             System.out.println("You lost, Better luck next time!");
 
         }
-
-
-        //Restarts the game
-
+        //
         do {
             Scanner gameInput = new Scanner(System.in);
-            System.out.println("Would you like to play again? \n " + "1 = Yes \n " + " 2 = No");
+            System.out.println("Would you like to play again? \n " + "1 = Yes \n " + "2 = No");
             gameRestart = gameInput.nextInt();
 
             if (gameRestart == 1) {
-
-
                 System.out.println("Go get that computer tiger! ");
-                System.out.println(coreGameLoop);
+                System.out.println("");
+
 
             } else if (gameRestart == 2) {
-
                 System.out.println("Thanks for Playing! ");
                 break;
+            } else if (gameRestart != 1 && gameRestart != 2) {
+
+                System.out.println("You need to type either 1 or 2 please try again!");
 
             }
+
         } while (playerScore == 10 || computerScore == 10);
+        //
+
 
     }
-
-
-    //Test code here
-
-
-    /*public static String choices(int choice) {
-
-        switch (choice) {
-
-            case 0:
-                System.out.println("Rock");
-                break;
-
-            case 1:
-                System.out.println("Paper");
-                break;
-
-            case 2:
-                System.out.println("Scissors");
-                break;
-
-            default:
-                System.out.println("Invalid input try again");
-
-
-        }
-        return String.valueOf(choice);
-
-    }*/
 
     public static int playerWins(int input, int choiceOfComputer, int playerScore) {
 
@@ -148,26 +108,29 @@ public class Main {
             System.out.println("Rock beats scissors you win this round!");
             playerScore++;
 
+
         } else if (input == 1 && choiceOfComputer == 0) {
 
             System.out.println("Paper beats rock you win this round!");
             playerScore++;
+
 
         } else if (input == 2 && choiceOfComputer == 1) {
 
             System.out.println("Scissors beat paper you win this round!");
             playerScore++;
 
-            }
+
+        }
 
         return playerScore;
 
     }
 
 
-    public static int computerWins(int input, int choiceOfComputer, int computerScore){
+    public static int computerWins(int input, int choiceOfComputer, int computerScore) {
 
-         if (input == 0 && choiceOfComputer == 1) {
+        if (input == 0 && choiceOfComputer == 1) {
 
             System.out.println("Paper beats rock you lose this round!");
             computerScore++;
@@ -188,7 +151,7 @@ public class Main {
         }
 
 
-    return computerScore;
+        return computerScore;
 
     }
 
